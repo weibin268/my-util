2dbd3eea3c85c1047b0586dbf0faf00f8cdb63c6
庄伟斌 <zhuangweibin@qq.com>
2020-11-20 11:30:38 +0800

    aa

--- src/test/java/com/zhuang/hutool/FileUtilTest.java
+++ src/test/java/com/zhuang/hutool/FileUtilTest.java
@@ -1,6 +1,7 @@
 package com.zhuang.hutool;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
 import org.junit.Test;
 
 public class FileUtilTest {
@@ -9,6 +10,13 @@ public class FileUtilTest {
     public void test() {
         //System.out.println(System.getProperty("user.dir"));
         FileUtil.readUtf8Lines(System.getProperty("user.dir") + "/src/test/java/com/zhuang/hutool/FileUtilTest.java").forEach(System.out::println);
+    }
+
+    @Test
+    public void test2() {
+        FileUtil.readUtf8Lines(this.getClass().getResource("/txt/test.txt")).forEach(System.out::println);
+        System.out.println("--------------------------------------------------------------------");
+        FileUtil.readUtf8Lines(ResourceUtil.getResource("txt/test.txt")).forEach(System.out::println);
     }
 
 }
