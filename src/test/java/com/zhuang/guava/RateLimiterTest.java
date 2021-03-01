5f2f59480d38794e65028e2aee02a6a11a1d9b3a
庄伟斌 <zhuangweibin@qq.com>
2021-03-01 16:07:28 +0800

    aa

--- src/test/java/com/zhuang/guava/RateLimiterTest.java
+++ src/test/java/com/zhuang/guava/RateLimiterTest.java
@@ -1,12 +1,23 @@
 package com.zhuang.guava;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.junit.Test;
 
 public class RateLimiterTest {
 
-    @Test
-    public void test() {
+    //每秒生产5个令牌
+    private static RateLimiter limiter = RateLimiter.create(5);
 
+    @Test
+    public void test() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(100);
+            if (limiter.tryAcquire()) {
+                System.out.println("执行成功！");
+            } else {
+                System.out.println("系统繁忙！");
+            }
+        }
     }
 
 }
