48f85aa812689939dd46ec682caa398b442ed384
庄伟斌 <zhuangweibin@qq.com>
2021-07-06 11:58:39 +0800

    aa

--- src/main/java/com/zhuang/util/BigDecimalUtils.java
+++ src/main/java/com/zhuang/util/BigDecimalUtils.java
@@ -9,42 +9,101 @@ import java.util.stream.Collectors;
 
 public class BigDecimalUtils {
 
+    // 默认保留小数位数
     private static final int DEFAULT_SCALE = 4;
 
+    /**
+     * 加
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static BigDecimal add(BigDecimal a, BigDecimal b) {
+        if (a == null || b == null) return null;
+        return a.add(b);
+    }
+
+    /**
+     * 减
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        if (a == null || b == null) return null;
+        return a.subtract(b);
+    }
+
+    /**
+     * 乘
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        if (a == null || b == null) return null;
+        return a.multiply(b);
+    }
+
+    /**
+     * 除
+     *
+     * @param a
+     * @param b
+     * @return
+     */
     public static BigDecimal divide(BigDecimal a, BigDecimal b) {
         return divide(a, b, DEFAULT_SCALE, RoundingMode.HALF_UP);
     }
 
+    /**
+     * 除
+     *
+     * @param a
+     * @param b
+     * @param scale
+     * @return
+     */
     public static BigDecimal divide(BigDecimal a, BigDecimal b, int scale) {
         return divide(a, b, scale, RoundingMode.HALF_UP);
     }
 
+    /**
+     * 除
+     *
+     * @param a
+     * @param b
+     * @param scale
+     * @param roundingMode
+     * @return
+     */
     public static BigDecimal divide(BigDecimal a, BigDecimal b, int scale, RoundingMode roundingMode) {
         if (a == null || b == null) return null;
         if (b.compareTo(BigDecimal.ZERO) == 0) return null;
         return a.divide(b, scale, roundingMode);
     }
 
-    public static BigDecimal multiply(BigDecimal a, BigDecimal b) {
-        if (a == null || b == null) return null;
-        return a.multiply(b);
-    }
-
-    public static BigDecimal subtract(BigDecimal a, BigDecimal b) {
-        if (a == null || b == null) return null;
-        return a.subtract(b);
-    }
-
-    public static BigDecimal add(BigDecimal a, BigDecimal b) {
-        if (a == null || b == null) return null;
-        return a.add(b);
-    }
-
+    /**
+     * 判断两个数是否相等
+     *
+     * @param a
+     * @param b
+     * @return
+     */
     public static boolean equals(BigDecimal a, BigDecimal b) {
         if (a == null || b == null) return false;
         return a.compareTo(b) == 0;
     }
 
+    /**
+     * 判断一个数是否等于0
+     *
+     * @param a
+     * @return
+     */
     public static boolean equalsZero(BigDecimal a) {
         if (a == null) return false;
         return a.compareTo(BigDecimal.ZERO) == 0;
@@ -98,26 +157,65 @@ public class BigDecimalUtils {
         return num.setScale(scale, roundingMode);
     }
 
+    /**
+     * 求和
+     *
+     * @param numList
+     * @return
+     */
     public static BigDecimal sum(Collection<BigDecimal> numList) {
         return numList.stream().filter(Objects::nonNull).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
+    /**
+     * 最小值
+     *
+     * @param numList
+     * @return
+     */
     public static BigDecimal min(Collection<BigDecimal> numList) {
         return numList.stream().filter(Objects::nonNull).min(BigDecimal::compareTo).orElse(null);
     }
 
+    /**
+     * 最大值
+     *
+     * @param numList
+     * @return
+     */
     public static BigDecimal max(Collection<BigDecimal> numList) {
         return numList.stream().filter(Objects::nonNull).max(BigDecimal::compareTo).orElse(null);
     }
 
+    /**
+     * 平均值
+     *
+     * @param numList
+     * @return
+     */
     public static BigDecimal avg(Collection<BigDecimal> numList) {
         return avg(numList, DEFAULT_SCALE);
     }
 
+    /**
+     * 平均值
+     *
+     * @param numList
+     * @param scale
+     * @return
+     */
     public static BigDecimal avg(Collection<BigDecimal> numList, int scale) {
         return avg(numList, scale, RoundingMode.HALF_UP);
     }
 
+    /**
+     * 平均值
+     *
+     * @param numList
+     * @param scale
+     * @param roundingMode
+     * @return
+     */
     public static BigDecimal avg(Collection<BigDecimal> numList, int scale, RoundingMode roundingMode) {
         numList = numList.stream().filter(Objects::nonNull).collect(Collectors.toList());
         int size = numList.size();
@@ -127,11 +225,23 @@ public class BigDecimalUtils {
 
     /**
      * 获取小数部分
+     *
      * @param num
      * @return
      */
-    public BigDecimal getFraction(BigDecimal num){
+    public BigDecimal getFraction(BigDecimal num) {
         return num.remainder(BigDecimal.ONE);
+    }
+
+    /**
+     * 转字符串，去小数多余零，去科学计数法
+     *
+     * @param num
+     * @return
+     */
+    public String toString(BigDecimal num) {
+        if (num == null) return null;
+        return num.stripTrailingZeros().toPlainString();
     }
 
 }
