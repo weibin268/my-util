26d3e869e7f812b76fc63743c528e266b85e5e11
weibin268 <binweizhuang@hotmail.com>
2021-07-26 22:14:46 +0800

    aa

--- src/test/java/com/zhuang/util/ThumbnailUtilsTest.java
+++ src/test/java/com/zhuang/util/ThumbnailUtilsTest.java
@@ -2,13 +2,14 @@ package com.zhuang.util;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class ThumbnailUtilsTest {
 
     @Test
-    public void setScaleAndQuality() {
-
+    public void setScaleAndQuality() throws FileNotFoundException {
+        ThumbnailUtils.setScaleAndQuality(getClass().getResourceAsStream("/images/test.jpg"), new FileOutputStream("D:\\temp\\images\\test.jpg"), 0.5, 0.5);
     }
 
 }
