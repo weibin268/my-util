61b4fba1618f08bb764a93fe34dee299e60ee0f7
weibin268 <binweizhuang@hotmail.com>
2021-08-10 23:43:20 +0800

    aa

--- src/main/java/com/zhuang/util/fastjson/FastjsonUtils.java
+++ src/main/java/com/zhuang/util/fastjson/FastjsonUtils.java
@@ -1,10 +1,9 @@
 package com.zhuang.util.fastjson;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class FastjsonUtils {
-    
+
     public static <T> T toBean(String json, Class<T> clazz) {
         return JSON.parseObject(json, clazz);
     }
