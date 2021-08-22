90edc16e16a2201cfb941fc3923226c08bf7c98d
weibin268 <binweizhuang@hotmail.com>
2021-08-22 18:33:33 +0800

    aa

--- src/main/java/com/zhuang/util/PoiUtils.java
+++ src/main/java/com/zhuang/util/PoiUtils.java
@@ -10,25 +10,43 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PoiUtils {
 
-    public static InputStream hiddenSheet(InputStream inputStream, List<Integer> sheetIndexList) {
-        return hiddenSheet(inputStream, sheetIndexList, false);
+    public static InputStream hiddenSheetByNames(InputStream inputStream, List<String> sheetNameList) {
+        if (sheetNameList == null || sheetNameList.size() == 0) {
+            return inputStream;
+        }
+        Workbook workbook = WorkbookUtil.createBook(inputStream);
+        List<Integer> sheetIndexList = sheetNameList.stream().map(c -> workbook.getSheetIndex(c)).collect(Collectors.toList());
+        return hiddenSheetByIndexes(workbook, false, sheetIndexList);
     }
 
-    public static InputStream hiddenSheet(InputStream inputStream, List<Integer> sheetIndexList, boolean veryHidden) {
+    public static InputStream hiddenSheetByIndexes(InputStream inputStream, List<Integer> sheetIndexList) {
         if (sheetIndexList == null || sheetIndexList.size() == 0) {
             return inputStream;
         }
         Workbook workbook = WorkbookUtil.createBook(inputStream);
+        return hiddenSheetByIndexes(workbook, false, sheetIndexList);
+    }
+
+    private static InputStream hiddenSheetByIndexes(Workbook workbook, boolean veryHidden, List<Integer> sheetIndexList) {
+        sheetIndexList.sort(Integer::compareTo);
         sheetIndexList.forEach(index -> {
             if (veryHidden) {
-                //该方法隐藏后用户不可以取消隐藏
+                //隐藏后用户不可以取消隐藏
                 workbook.setSheetVisibility(index, SheetVisibility.VERY_HIDDEN);
             } else {
-                //该方法第一个sheet隐藏不了
-                workbook.setSheetHidden(index, true);
+                if (index == workbook.getActiveSheetIndex()) {
+                    int activeIndex = 0;
+                    if (index < (workbook.getNumberOfSheets() - 1)) {
+                        activeIndex = index + 1;
+                    }
+                    workbook.setActiveSheet(activeIndex);
+                }
+                //隐藏后用户可以取消隐藏（注：当sheet为活动的时隐藏不了）
+                workbook.setSheetVisibility(index, SheetVisibility.HIDDEN);
             }
         });
         return workbookToInputStream(workbook);
--- src/test/java/com/zhuang/util/PoiUtilsTest.java
+++ src/test/java/com/zhuang/util/PoiUtilsTest.java
@@ -1,13 +1,20 @@
 package com.zhuang.util;
 
+import cn.hutool.core.io.FileUtil;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class PoiUtilsTest {
 
     @Test
-    public void removeSheet() {
-        
+    public void removeSheet() throws IOException {
+        InputStream resourceAsStream = getClass().getResourceAsStream("/excel/test.xlsx");
+        //InputStream inputStream = PoiUtils.hiddenSheetByIndexes(resourceAsStream, Arrays.asList(0));
+        InputStream inputStream = PoiUtils.hiddenSheetByNames(resourceAsStream, Arrays.asList("sheet1"));
+        FileUtil.writeFromStream(inputStream, new File("D:\\temp\\test-out.xlsx"));
     }
 }
