ba26496d556cc6b3e42af65c971226e5112b20e7
zwb <binweizhuang@hotmail.com>
2021-12-26 11:44:22 +0800

    aa

--- src/test/java/com/zhuang/hutool/FileUtilTest.java
+++ src/test/java/com/zhuang/hutool/FileUtilTest.java
@@ -4,10 +4,11 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
 
 public class FileUtilTest {
 
