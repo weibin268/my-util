0be38e1355274495b2af4003c2941321cd3c02b6
庄伟斌 <zhuangweibin@qq.com>
2022-01-18 17:00:44 +0800

    aa

--- src/test/java/com/zhuang/hutool/ExcelUtilTest.java
+++ src/test/java/com/zhuang/hutool/ExcelUtilTest.java
@@ -1,10 +1,16 @@
 package com.zhuang.hutool;
 
 import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import cn.hutool.poi.excel.sax.handler.RowHandler;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ExcelUtilTest {
 
@@ -18,4 +24,24 @@ public class ExcelUtilTest {
         });
     }
 
+
+    @Test
+    public void test() {
+        List<Map<String, Object>> userList = new ArrayList<>();
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "zwb");
+        user.put("age", 11);
+        userList.add(user);
+        user = new HashMap<>();
+        user.put("name", "lxc1111111111111111111111111111111");
+        user.put("age", 12);
+        userList.add(user);
+        final ExcelWriter writer = ExcelUtil.getWriter("D:\\temp\\test.xlsx");
+        final Font headFont = writer.getWorkbook().createFont();
+        headFont.setBold(true);
+        writer.addHeaderAlias("name", "姓名");
+        writer.getHeadCellStyle().setFont(headFont);
+        writer.setColumnWidth(1, 100);
+        writer.write(userList).flush();
+    }
 }
