ed5c3c08ab02e4b5b62d0dc926acc9f184e788f9
庄伟斌 <zhuangweibin@qq.com>
2022-08-02 11:22:32 +0800

    aa

--- src/main/java/com/zhuang/util/tree/TreeUtils.java
+++ src/main/java/com/zhuang/util/tree/TreeUtils.java
@@ -14,7 +14,9 @@ public class TreeUtils {
         List<T> newNodeList = new ArrayList<>();
         nodeList = nodeList.stream().sorted(Comparator.comparing(T::getNodeCode, Comparator.nullsLast(String::compareTo))).collect(Collectors.toList());
         for (T node : nodeList) {
-            T parent = recursiveFindParent4Code(newNodeList, node);
+            List<T> parentList = new ArrayList<>();
+            recursiveFindParent4Code(newNodeList, node, parentList);
+            T parent = CollectionUtils.isEmpty(parentList) ? null : parentList.get(parentList.size() - 1);
             if (parent != null) {
                 parent.getChildren().add(node);
             } else {
@@ -37,17 +39,15 @@ public class TreeUtils {
         return newNodeList;
     }
 
-    private static <T extends TreeNode4Code> T recursiveFindParent4Code(List<T> tree, T node) {
+    private static <T extends TreeNode4Code> void recursiveFindParent4Code(List<T> tree, T node, List<T> result) {
         for (T parent : tree) {
             if (node.getNodeCode().startsWith(parent.getNodeCode()) && !node.getNodeCode().equals(parent.getNodeCode())) {
-                return parent;
+                result.add(parent);
             }
             if (!CollectionUtils.isEmpty(parent.getChildren())) {
-                T p = recursiveFindParent4Code(parent.getChildren(), node);
-                if (p != null) return p;
+                recursiveFindParent4Code(parent.getChildren(), node, result);
             }
         }
-        return null;
     }
 
     private static <T extends TreeNode4Id> T findParent4Id(List<T> tree, T node) {
@@ -58,4 +58,5 @@ public class TreeUtils {
         }
         return null;
     }
+
 }
