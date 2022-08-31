b8bd837bdf3482236c0e423ab2a6119688fd58dc
庄伟斌 <zhuangweibin@qq.com>
2022-08-31 10:55:06 +0800

    aa

--- src/test/java/com/zhuang/util/JwtUtilsTest.java
+++ src/test/java/com/zhuang/util/JwtUtilsTest.java
@@ -2,6 +2,8 @@ package com.zhuang.util;
 
 import cn.hutool.core.date.DateUtil;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
 import org.junit.Test;
 
 import java.util.Date;
@@ -21,9 +23,15 @@ public class JwtUtilsTest {
 
     @Test
     public void parseToken() {
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyTmFtZSI6Inp3YiIsImV4cCI6MTY2MTg3MjM1MCwidXNlcklkIjoiYWJjIn0.8_Xl0fZrGZqzUajss6yfqEFxxELjkpOtH3-olQGOqEx0yf0hdh1xPnUY9w54FL8puVsmj5rYKtDCzjv1TE__5g";
-        Claims claims = JwtUtils.parseToken(token);
-        System.out.println(claims);
+        String token = "eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyTmFtZSI6Inp3YiIsImV4cCI6MTY2MTkxNDUzMywidXNlcklkIjoiYWJjIn0.ghZC_MuL-AN_WLqZr7o_HMV04SZc46zOc9IF-XPV5r-SSnmAvV9iWZYIAUsYNi6YxHe6MRBvcKqbq_uGnqdzhA";
+        try {
+            Claims claims = JwtUtils.parseToken(token);
+            System.out.println(claims);
+        } catch (SignatureException signatureException) {
+            System.out.println("token 签名异常");
+        } catch (ExpiredJwtException expiredJwtException) {
+            System.out.println("token 过期！");
+        }
     }
 
 }
