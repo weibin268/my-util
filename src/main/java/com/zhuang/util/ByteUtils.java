ed286992702c37ed1d5648c71efdcbe52ffecc6c
zwb <binweizhuang@hotmail.com>
2022-10-17 22:46:28 +0800

    aa

--- src/main/java/com/zhuang/util/ByteUtils.java
+++ src/main/java/com/zhuang/util/ByteUtils.java
@@ -9,4 +9,11 @@ public class ByteUtils {
         return ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).getFloat();
     }
 
+    public static byte[] getBytes(float value) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4);
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putFloat(value);
+        return byteBuffer.array();
+    }
+
 }
