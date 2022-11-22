0eec79cc0ce8cfd8eb5e7e14b0c31035bc8733ca
zwb <binweizhuang@hotmail.com>
2022-11-22 15:45:41 +0800

    aa

--- src/main/java/com/zhuang/util/WaterMeasureUtils.java
+++ src/main/java/com/zhuang/util/WaterMeasureUtils.java
@@ -2,6 +2,7 @@ package com.zhuang.util;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import io.jsonwebtoken.lang.Collections;
 import lombok.Data;
 import org.springframework.util.CollectionUtils;
 
@@ -18,6 +19,12 @@ import java.util.stream.Collectors;
  */
 public class WaterMeasureUtils {
 
+    /**
+     * 计算平均值
+     *
+     * @param dataList
+     * @return
+     */
     public static BigDecimal calcAvg4Water(List<? extends WaterData> dataList) {
 
         dataList = dataList.stream()
@@ -63,7 +70,12 @@ public class WaterMeasureUtils {
         return result;
     }
 
-
+    /**
+     * 计算平均值
+     *
+     * @param dataList
+     * @return
+     */
     public static BigDecimal calcAvg4Normal(List<? extends WaterData> dataList) {
         dataList = dataList.stream()
                 .filter(c -> c.getDataTime() != null && c.getDataValue() != null)
@@ -92,10 +104,69 @@ public class WaterMeasureUtils {
         return false;
     }
 
+
+    /**
+     * 平滑数据
+     *
+     * @param dataList
+     * @param leftCount
+     * @param rightCount
+     */
+    public static void smoothDataList(List<? extends WaterData> dataList, int leftCount, int rightCount) {
+        smoothDataList(dataList, leftCount, rightCount, null, null);
+    }
+
+    /**
+     * 平滑数据
+     *
+     * @param dataList
+     * @param leftCount
+     * @param rightCount
+     */
+    public static void smoothDataList(List<? extends WaterData> dataList, int leftCount, int rightCount, List<? extends WaterData> extLeftDataList, List<? extends WaterData> extRightDataList) {
+        for (int i = 0; i < dataList.size(); i++) {
+            WaterData waterData = dataList.get(i);
+            if (waterData.getDataValue() == null) continue;
+            List<WaterData> sampleList = new ArrayList<>();
+            sampleList.add(waterData);
+            for (int j = 1; j <= leftCount; j++) {
+                int sampleIndex = i - j;
+                if (sampleIndex >= 0) {
+                    sampleList.add(dataList.get(sampleIndex));
+                } else if (!Collections.isEmpty(extLeftDataList)) {
+                    sampleIndex = extLeftDataList.size() - Math.abs(sampleIndex);
+                    if (sampleIndex >= 0) {
+                        sampleList.add(extLeftDataList.get(sampleIndex));
+                    }
+                }
+            }
+            for (int j = 1; j <= rightCount; j++) {
+                int sampleIndex = i + j;
+                if (sampleIndex < dataList.size()) {
+                    sampleList.add(dataList.get(sampleIndex));
+                } else if (!Collections.isEmpty(extRightDataList)) {
+                    sampleIndex = extRightDataList.size() - Math.abs(sampleIndex);
+                    if (sampleIndex >= 0) {
+                        sampleList.add(extRightDataList.get(sampleIndex));
+                    }
+                }
+            }
+            sampleList = sampleList.stream().filter(c -> c.getDataValue() != null).collect(Collectors.toList());
+            if (CollectionUtils.isEmpty(sampleList)) continue;
+            BigDecimal avgValue = sampleList.stream().map(c -> c.getDataValue()).reduce(BigDecimal.ZERO, BigDecimal::add).divide(BigDecimal.valueOf(sampleList.size()), 3, RoundingMode.HALF_UP);
+            if (avgValue != null) {
+                waterData.setDataValue(avgValue);
+            }
+        }
+    }
+
     public interface WaterData {
         Date getDataTime();
 
         BigDecimal getDataValue();
+
+        default void setDataValue(BigDecimal dataValue) {
+        }
     }
 
     @Data
--- src/test/java/com/zhuang/util/WaterMeasureUtilsTest.java
+++ src/test/java/com/zhuang/util/WaterMeasureUtilsTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class WaterMeasureUtilsTest {
 
     @Test
-    public void test() {
+    public void calcAvg4Water() {
         List<WaterMeasureUtils.WaterData> dataList = new ArrayList<>();
 
 //        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:00:00"), new BigDecimal("11.1")));
@@ -38,6 +38,32 @@ public class WaterMeasureUtilsTest {
 
     }
 
+    @Test
+    public void smoothDataList() {
+        List<WaterMeasureUtils.WaterData> dataList = new ArrayList<>();
+
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:00:00"), new BigDecimal("11.1")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:05:00"), new BigDecimal("22.2")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:10:00"), new BigDecimal("33.3")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:15:00"), new BigDecimal("44.44")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:20:00"), new BigDecimal("55.5")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:25:00"), new BigDecimal("66.6")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:30:00"), new BigDecimal("77.7")));
+        dataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:35:00"), new BigDecimal("88.8")));
+
+        List<WaterMeasureUtils.WaterData> leftDataList = new ArrayList<>();
+        leftDataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:00:00"), new BigDecimal("11.1")));
+        leftDataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:05:00"), new BigDecimal("22.2")));
+
+        List<WaterMeasureUtils.WaterData> rightDataList = new ArrayList<>();
+        rightDataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:00:00"), new BigDecimal("11.1")));
+        rightDataList.add(new MyWaterData(DateUtil.parseDateTime("2022-11-01 00:05:00"), new BigDecimal("22.2")));
+
+
+        WaterMeasureUtils.smoothDataList(dataList, 2, 2, leftDataList, rightDataList);
+        System.out.println(dataList);
+    }
+
     @Data
     public static class MyWaterData implements WaterMeasureUtils.WaterData {
 
@@ -57,6 +83,11 @@ public class WaterMeasureUtilsTest {
         @Override
         public BigDecimal getDataValue() {
             return this.dataValue;
+        }
+
+        @Override
+        public void setDataValue(BigDecimal dataValue) {
+            this.dataValue = dataValue;
         }
     }
 
