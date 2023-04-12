b8f7d779de8965e41e2193a20c091d6079d35a31
庄伟斌 <zhuangweibin@qq.com>
2023-04-12 10:55:15 +0800

    aa

--- src/main/java/com/zhuang/util/UrlQueryUtils.java
+++ src/main/java/com/zhuang/util/UrlQueryUtils.java
@@ -3,6 +3,7 @@ package com.zhuang.util;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.net.url.UrlQuery;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class UrlQueryUtils {
@@ -13,5 +14,10 @@ public class UrlQueryUtils {
         } else {
             return UrlQuery.of(BeanUtil.beanToMap(obj)).toString();
         }
+    }
+
+    public static Map<CharSequence, CharSequence> getQueryMap(String queryString) {
+        Map<CharSequence, CharSequence> queryMap = UrlQuery.of(queryString, StandardCharsets.UTF_8).getQueryMap();
+        return queryMap;
     }
 }
--- src/test/java/com/zhuang/util/UrlQueryUtilsTest.java
+++ src/test/java/com/zhuang/util/UrlQueryUtilsTest.java
@@ -8,11 +8,16 @@ import java.util.Map;
 public class UrlQueryUtilsTest {
 
     @Test
-    public void test() {
+    public void getQueryString() {
         Map<String, Object> params = new HashMap<>();
         params.put("a", 123);
         params.put("b", "abc");
         System.out.println(UrlQueryUtils.getQueryString(params));
     }
 
+    @Test
+    public void getQueryMap() {
+        Map<CharSequence, CharSequence> queryMap = UrlQueryUtils.getQueryMap("name=庄伟斌&age=18");
+        System.out.println(queryMap);
+    }
 }
