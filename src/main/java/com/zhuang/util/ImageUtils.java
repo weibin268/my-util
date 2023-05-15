7e8889865c6a2f6867a2db7ad39b4861ee2c929e
庄伟斌 <zhuangweibin@qq.com>
2023-05-15 16:42:45 +0800

    aa

--- src/main/java/com/zhuang/util/ImageUtils.java
+++ src/main/java/com/zhuang/util/ImageUtils.java
@@ -1,10 +1,14 @@
 package com.zhuang.util;
 
+import javax.net.ssl.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 
 public class ImageUtils {
@@ -15,6 +19,15 @@ public class ImageUtils {
         try {
             URL url = new URL(path);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            if (connection instanceof HttpsURLConnection) {
+                SSLContext sslcontext = SSLContext.getInstance("SSL", "SunJSSE");//第一个参数为协议,第二个参数为提供者(可以缺省)
+                TrustManager[] tm = {new MyX509TrustManager()};
+                sslcontext.init(null, tm, new SecureRandom());
+
+                HttpsURLConnection connection4Https = (HttpsURLConnection) connection;
+                connection4Https.setHostnameVerifier((hostname, session) -> true);
+                connection4Https.setSSLSocketFactory(sslcontext.getSocketFactory());
+            }
             connection.setRequestMethod("GET");
             connection.setConnectTimeout(5000);
             connection.setReadTimeout(5000);
@@ -38,4 +51,27 @@ public class ImageUtils {
         }
         return Base64.getEncoder().encodeToString(out.toByteArray());
     }
+
+    public static class MyX509TrustManager implements X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
+
 }
--- src/test/java/com/zhuang/util/ImageUtilsTest.java
+++ src/test/java/com/zhuang/util/ImageUtilsTest.java
@@ -8,7 +8,7 @@ public class ImageUtilsTest {
 
     @Test
     public void get(){
-        String base64ByUrl = ImageUtils.getBase64ByUrl("https://fastdfs-gateway.ys7.com/c9e5/1/capture/003j0m5Qee8XBsSkx180PrX1M9AE3sC.jpg?Expires=1684201112&OSSAccessKeyId=LTAIzI38nEHqg64n&Signature=dqM6sH9Gu8sEoUMwbeVDeb7EcQ4%3D");
+        String base64ByUrl = ImageUtils.getBase64ByUrl("https://192.168.2.5/evo-pic/f535bf07-5835-11ec-98bc-2cea7fd38988/20230514/1/dsf_8acd3c37-f229-11ed-a5ee-2cea7fd38988_27669558_27787919.jpg?token=1:6f1dc011-ca80-4b40-be40-8ed0ccc24c46&oss_addr=192.168.2.5:8925");
         System.out.println(base64ByUrl);
     }
 
