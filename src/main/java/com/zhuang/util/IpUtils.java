146ef05e69e880624bfbbc14720617b6296aba8e
庄伟斌 <zhuangweibin@qq.com>
2023-06-07 09:30:48 +0800

    aa

--- src/main/java/com/zhuang/util/IpUtils.java
+++ src/main/java/com/zhuang/util/IpUtils.java
@@ -1,6 +1,7 @@
 package com.zhuang.util;
 
 import cn.hutool.core.lang.PatternPool;
+import cn.hutool.core.util.StrUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,11 +10,13 @@ import java.util.regex.Matcher;
 public class IpUtils {
 
     public static String replaceIp(String str, String newIp) {
+        if (StrUtil.isEmpty(str) || StrUtil.isEmpty(newIp)) return str;
         return PatternPool.IPV4.matcher(str).replaceAll(newIp);
     }
 
     public static List<String> getIpList(String str) {
         List<String> result = new ArrayList<>();
+        if (StrUtil.isEmpty(str)) return result;
         Matcher matcher = PatternPool.IPV4.matcher(str);
         while (matcher.find()) {
             result.add(matcher.group(0));
@@ -25,5 +28,6 @@ public class IpUtils {
         List<String> ipList = getIpList(str);
         return ipList.size() > 0 ? ipList.get(0) : null;
     }
+
 
 }
