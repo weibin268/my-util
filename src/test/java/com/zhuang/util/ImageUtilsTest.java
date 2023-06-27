f05aa8138976f1498e99dbbad0560f44d01f6a8b
zwb <binweizhuang@hotmail.com>
2023-06-27 18:10:04 +0800

    aa

--- src/test/java/com/zhuang/util/ImageUtilsTest.java
+++ src/test/java/com/zhuang/util/ImageUtilsTest.java
@@ -10,7 +10,8 @@ public class ImageUtilsTest {
 
     @Test
     public void getBase64ByUrl() {
-        String base64ByUrl = ImageUtils.getBase64ByUrl("https://192.168.2.5/evo-pic/f535bf07-5835-11ec-98bc-2cea7fd38988/20230514/1/dsf_8acd3c37-f229-11ed-a5ee-2cea7fd38988_27669558_27787919.jpg?token=1:6f1dc011-ca80-4b40-be40-8ed0ccc24c46&oss_addr=192.168.2.5:8925");
+        String path = "https://pics1.baidu.com/feed/d62a6059252dd42aba8f801aaabe38b9c8eab80b.jpeg@f_auto?token=8b58cc25e5943bf751a0fc4aefda2007";
+        String base64ByUrl = ImageUtils.getBase64ByUrl(path);
         System.out.println(base64ByUrl);
     }
 
