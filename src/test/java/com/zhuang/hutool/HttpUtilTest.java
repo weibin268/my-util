7f3397e9634bcea49ad0ece59e50535df20e7802
庄伟斌 <zhuangweibin@qq.com>
2023-07-14 17:32:07 +0800

    aa

--- src/test/java/com/zhuang/hutool/HttpUtilTest.java
+++ src/test/java/com/zhuang/hutool/HttpUtilTest.java
@@ -1,14 +1,32 @@
 package com.zhuang.hutool;
 
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import org.junit.Test;
 
 public class HttpUtilTest {
 
     @Test
-    public void test() {
+    public void get() {
         String result = HttpUtil.get("https://www.baidu.com");
         System.out.println(result);
     }
 
+    @Test
+    public void createPost4Form() {
+        HttpRequest post = HttpUtil.createPost("http://localhost:8888");
+        post.header("token", "test");
+        post.form("name", "zwb");
+        String body = post.execute().body();
+        System.out.println(body);
+    }
+
+    @Test
+    public void createPost4Json() {
+        HttpRequest post = HttpUtil.createPost("http://localhost:8888");
+        post.header("token", "test");
+        String body = post.body("{age:1}").execute().body();
+        System.out.println(body);
+
+    }
 }
