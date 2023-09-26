7cd4cf23e17f540461dc2ae44dddb70b4a453004
zwb <binweizhuang@hotmail.com>
2023-09-26 12:14:28 +0800

    aa

--- src/main/java/com/zhuang/util/IpUtils.java
+++ src/main/java/com/zhuang/util/IpUtils.java
@@ -2,6 +2,10 @@ package com.zhuang.util;
 
 import cn.hutool.core.lang.PatternPool;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +33,37 @@ public class IpUtils {
         return ipList.size() > 0 ? ipList.get(0) : null;
     }
 
+    public static IPLocation getLocationByIp(String ip) {
+        try {
+            IPLocation result = new IPLocation();
+            String s = HttpUtil.get("https://qifu-api.baidubce.com/ip/geo/v1/district?ip=" + ip);
+            JSONObject jsonObject = JSON.parseObject(s);
+            JSONObject data = jsonObject.getJSONObject("data");
+            if (data != null) {
+                result.setIsp(data.get("isp") != null ? data.get("isp").toString() : null);
+                result.setCountry(data.get("country") != null ? data.get("country").toString() : null);
+                result.setProv(data.get("prov") != null ? data.get("prov").toString() : null);
+                result.setCity(data.get("city") != null ? data.get("city").toString() : null);
+                result.setDistrict(data.get("district") != null ? data.get("district").toString() : null);
+            }
+            return result;
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    @Data
+    public static class IPLocation {
+        // 网络运营商
+        private String isp;
+        // 国家
+        private String country;
+        // 省份
+        private String prov;
+        // 城市
+        private String city;
+        // 区域
+        private String district;
+    }
 
 }
