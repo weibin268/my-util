e5f063ccec65fba2f8d6bf5c25fc0ea690f2423b
zwb <binweizhuang@hotmail.com>
2024-03-24 09:02:41 +0800

    aa

---
+++ src/test/java/com/zhuang/util/PathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.zhuang.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PathUtilsTest {
+
+    @Test
+    public void test(){
+        System.out.println(PathUtils.combine("aaa/","/bbb"));
+    }
+
+}
