c2d0d4b62d8b1ba366dc0f12570b1f0723906a5c
庄伟斌 <zhuangweibin@qq.com>
2024-04-27 00:57:50 +0800

    aa

--- src/test/java/com/zhuang/jna/JnaTest.java
+++ src/test/java/com/zhuang/jna/JnaTest.java
@@ -1,18 +1,19 @@
 package com.zhuang.jna;
 
+import com.sun.jna.Library;
 import com.sun.jna.Native;
 import org.junit.Test;
 
 public class JnaTest {
     @Test
-    public void test() {
-        System.setProperty("jna.library.path", "/Users/zhuang/Documents/temp/");
-        MyLib instance = (MyLib) Native.loadLibrary("test", MyLib.class);
-        int add = instance.add(1, 2);
-        System.out.println(add);
+    public void DeleteFileA() {
+        //System.setProperty("jna.library.path", "d:\\temp\\c");
+        Kernel32 kernel32 = (Kernel32) Native.loadLibrary("kernel32", Kernel32.class);
+        boolean b = kernel32.DeleteFileA("d:\\tmp\\c\\test.so");
+        System.out.println(b);
     }
 
-    public interface MyLib {
-        int add(int a, int b);
+    public interface Kernel32 extends Library {
+        boolean DeleteFileA(String fileName);
     }
 }
