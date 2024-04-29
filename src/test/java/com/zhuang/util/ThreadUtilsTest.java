8b550575644c1b71ed6604406307ade914b0d97a
庄伟斌 <zhuangweibin@qq.com>
2024-04-29 15:50:33 +0800

    aa

--- src/test/java/com/zhuang/util/ThreadUtilsTest.java
+++ src/test/java/com/zhuang/util/ThreadUtilsTest.java
@@ -2,40 +2,44 @@ package com.zhuang.util;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.stream.Collectors;
 
 public class ThreadUtilsTest {
 
     @Test
     public void startFutureTask() throws ExecutionException, InterruptedException {
-        FutureTask<String> futureTaskA = ThreadUtils.startFutureTask(() -> {
+        FutureTask<String> taskA = ThreadUtils.startFutureTask(() -> {
             ThreadUtils.sleep(3000);
             return "a";
         });
-
         ThreadUtils.sleep(3000);
         System.out.println("b");
-
-        String a = futureTaskA.get();
+        String a = taskA.get();
         System.out.println(a);
     }
 
     @Test
-    public void startCompletableFuture()   {
-        CompletableFuture<String> futureTaskA = ThreadUtils.startCompletableFuture(() -> {
-            System.out.println("begin a");
-            ThreadUtils.sleep(3000);
-            System.out.println("end a");
-            return "a";
-        });
-
-        ThreadUtils.sleep(3000);
-        System.out.println("b");
-
-        String a = futureTaskA.join();
-        System.out.println(a);
+    public void startCompletableFuture() {
+        List<CompletableFuture<String>> taskList = Arrays.asList("a", "b", "c").stream().map(c -> ThreadUtils.startCompletableFuture(() -> {
+            System.out.println(c + " begin");
+            if (c.equals("b")) {
+                ThreadUtils.sleep(3000);
+            }
+            System.out.println(c + " end");
+            return "result:" + c;
+        })).collect(Collectors.toList());
+        List<String> result = new ArrayList<>();
+        for (CompletableFuture<String> task : taskList) {
+            String taskResult = task.join();
+            result.add(taskResult);
+        }
+        result.forEach(System.out::println);
     }
 
 
