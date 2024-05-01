222e8be2cb7b76df251a493178521469214fb3ad
zwb <binweizhuang@hotmail.com>
2024-05-01 09:49:24 +0800

    aa

--- src/main/java/com/zhuang/util/thread/ThreadUtils.java
+++ src/main/java/com/zhuang/util/thread/ThreadUtils.java
@@ -16,6 +16,7 @@ public class ThreadUtils {
 
     public static <T> CompletableFuture<T> startCompletableFuture(Supplier<T> supplier) {
         // 设置每个任务创建一个线，默认使用的是ForkJoinPool线程池
+        // 注意：慎用该方式，因为使用数据库连接池时，通常会为每个线程分配一个数据库连接，如果线程数量过多，而连接池中的连接数量有限，那么可能会出现线程等待连接的情况
         return CompletableFuture.supplyAsync(supplier, new ThreadPerTaskExecutor());
     }
 
