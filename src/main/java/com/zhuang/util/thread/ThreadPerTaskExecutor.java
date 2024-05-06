9daae837c81ef42c42b853b5342a6e3e43d81498
zwb <binweizhuang@hotmail.com>
2024-05-06 10:49:44 +0800

    aa

--- src/main/java/com/zhuang/util/thread/ThreadPerTaskExecutor.java
+++ src/main/java/com/zhuang/util/thread/ThreadPerTaskExecutor.java
@@ -4,6 +4,10 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
+/**
+ * 每个任务创建一个线程
+ * 慎用：因为使用数据库连接池时，通常会为每个线程分配一个数据库连接，如果线程数量过多，而连接池中的连接数量有限，那么可能会出现线程等待连接的情况
+ */
 public final class ThreadPerTaskExecutor implements Executor {
     private final ThreadFactory threadFactory;
 
