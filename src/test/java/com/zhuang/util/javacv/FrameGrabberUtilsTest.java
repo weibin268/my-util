ddefc3f208415fb043a8698e8bc961f01b37a26f
庄伟斌 <zhuangweibin@qq.com>
2024-05-08 15:44:15 +0800

    aa

--- src/test/java/com/zhuang/util/javacv/FrameGrabberUtilsTest.java
+++ src/test/java/com/zhuang/util/javacv/FrameGrabberUtilsTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public class FrameGrabberUtilsTest {
     @Test
     public void test() {
-        FrameGrabberUtils.grab("rtmp://120.238.22.9:1935/live/openUrl/HnwpUas", "d:\\tmp\\test.mp4", 10);
+        FrameGrabberUtils.grab("rtmp://120.238.22.9:1935/live/openUrl/XfoWJ56", "d:\\tmp\\test.mp4", 10);
     }
 }
