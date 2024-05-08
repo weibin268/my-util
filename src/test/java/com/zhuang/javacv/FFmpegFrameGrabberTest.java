0d7bebcd04f9ce811a022890627579899643bb6a
庄伟斌 <zhuangweibin@qq.com>
2024-05-08 09:39:26 +0800

    aa

--- src/test/java/com/zhuang/javacv/FFmpegFrameGrabberTest.java
+++ src/test/java/com/zhuang/javacv/FFmpegFrameGrabberTest.java
@@ -1,9 +1,7 @@
 package com.zhuang.javacv;
 
 import org.bytedeco.ffmpeg.global.avcodec;
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FFmpegFrameRecorder;
-import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.*;
 import org.junit.Test;
 
 import java.io.File;
@@ -11,28 +9,35 @@ import java.io.File;
 public class FFmpegFrameGrabberTest {
 
     @Test
-    public void test() throws FFmpegFrameGrabber.Exception, FFmpegFrameRecorder.Exception, InterruptedException {
-        String filePath = "/Volumes/HD2/Temp/test.mp4";
-        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("rtsp://120.236.148.68:554/openUrl/uUoLBss");
+    public void test() throws FrameGrabber.Exception, FrameRecorder.Exception, InterruptedException {
+        String filePath = "d:\\tmp\\test.mp4";
+        int maxDurationSeconds = 30;
+        FFmpegFrameGrabber grabber = new FFmpegFrameGrabber("rtsp://120.238.22.9:554/openUrl/WAURUIw");
         grabber.setOption("rtsp_transport", "tcp");
-        grabber.start(); // 开始连接RTSP服务器
+        // 开始连接RTSP服务器
+        grabber.start();
         File outputFile = new File(filePath);
+        // 如果已存在相同文件，则删除该文件
         if (outputFile.exists()) {
-            outputFile.delete(); // 如果已存在相同文件，则删除该文件
+            outputFile.delete();
         }
         FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(filePath, grabber.getImageWidth(), grabber.getImageHeight());
         recorder.setAudioChannels(1);
         recorder.setVideoQuality(0);
-        recorder.setFrameRate(25);
+        recorder.setFrameRate(grabber.getFrameRate());
         recorder.setFormat("mp4");
         recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
-        recorder.start(); // 开始录制视频帧到文件
+        // 开始录制视频帧到文件
+        recorder.start();
         Frame frame;
-        while ((frame = grabber.grab()) != null) {
-            recorder.record(frame); // 将每一帧录制到文件
+        long startTime = System.currentTimeMillis();
+        while ((frame = grabber.grab()) != null && (System.currentTimeMillis() - startTime) / 1000 < maxDurationSeconds) {
+            // 将每一帧录制到文件
+            recorder.record(frame);
         }
-        Thread.sleep(1000 * 10);
-        recorder.stop(); // 停止录制视频帧到文件
-        grabber.stop(); // 停止连接RTSP服务器
+        // 停止录制视频帧到文件
+        recorder.close();
+        // 停止连接RTSP服务器
+        grabber.close();
     }
 }
