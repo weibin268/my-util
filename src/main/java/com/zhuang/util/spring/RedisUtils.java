98f36cdd50785c80d44f2dc6d519c0c17fb716df
庄伟斌 <zhuangweibin@qq.com>
2024-06-18 09:30:13 +0800

    aa

--- src/main/java/com/zhuang/util/spring/RedisUtils.java
+++ src/main/java/com/zhuang/util/spring/RedisUtils.java
@@ -1,5 +1,8 @@
 package com.zhuang.util.spring;
 
+import cn.hutool.core.util.StrUtil;
+import com.zhuang.util.jackson.JacksonUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
@@ -7,9 +10,12 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Component
 public class RedisUtils {
 
@@ -66,5 +72,23 @@ public class RedisUtils {
 
     public static Object getNativeConnection() {
         return _this.stringRedisTemplate.getConnectionFactory().getConnection().getNativeConnection();
+    }
+
+    public static List<String> getByPattern(String pattern) {
+        Set<String> keys = _this.stringRedisTemplate.keys(pattern);
+        return keys.stream().map(RedisUtils::get).collect(Collectors.toList());
+    }
+
+    public static <T> List<T> getByPattern(String pattern, Class<T> clazz) {
+        List<String> stringList = getByPattern(pattern);
+        return stringList.stream().filter(c -> StrUtil.isNotEmpty(c)).map(c -> {
+            T t = null;
+            try {
+                t = JacksonUtils.toBean(c, clazz);
+            } catch (Exception ex) {
+                log.error("RedisUtils.getByPattern error!", ex);
+            }
+            return t;
+        }).collect(Collectors.toList());
     }
 }
