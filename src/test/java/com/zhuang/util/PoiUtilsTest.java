a93eff67a2d1238d1cd5a4a5466e5bd1bc33aa20
zwb <binweizhuang@hotmail.com>
2024-07-18 21:52:14 +0800

    aa

--- src/test/java/com/zhuang/util/PoiUtilsTest.java
+++ src/test/java/com/zhuang/util/PoiUtilsTest.java
@@ -1,11 +1,17 @@
 package com.zhuang.util;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class PoiUtilsTest {
@@ -49,27 +55,54 @@ public class PoiUtilsTest {
     }
 
     @Test
-    public void handleEachCell() throws FileNotFoundException {
+    public void handleEachCell() throws IOException {
         InputStream resourceAsStream = getClass().getResourceAsStream("/excel/test.xlsx");
-        OutputStream outputStream = new FileOutputStream("d:\\temp\\test-out.xlsx");
+        OutputStream outputStream = Files.newOutputStream(Paths.get("d:\\temp\\test-out.xlsx"));
+        Map<String, CellStyle> cellStyleMap = new HashMap<>();
         PoiUtils.handleEachCell(resourceAsStream, outputStream, c -> {
-            if (c.getCell().getCellType().equals(CellType.NUMERIC)) {
-                double cellValue = Math.abs(c.getCell().getNumericCellValue());
-                CellStyle oldCellStyle = c.getCell().getCellStyle();
-                CellStyle newCellStyle = c.getWorkbook().createCellStyle();
-                newCellStyle.cloneStyleFrom(oldCellStyle);
-                c.getCell().setCellStyle(newCellStyle);
-                if (cellValue < 1) {
-                    c.getCell().getCellStyle().setDataFormat(c.getWorkbook().createDataFormat().getFormat("#0.000"));
-                } else if (cellValue < 10) {
-                    c.getCell().getCellStyle().setDataFormat(c.getWorkbook().createDataFormat().getFormat("#0.00"));
-                } else if (cellValue < 100) {
-                    c.getCell().getCellStyle().setDataFormat(c.getWorkbook().createDataFormat().getFormat("#0.0"));
-                } else {
-                    c.getCell().getCellStyle().setDataFormat(c.getWorkbook().createDataFormat().getFormat("#0"));
+            String dataFormatType = null;
+            String dataFormatTypeFlag = "dataFormatType:";
+            if (StrUtil.isNotEmpty(c.getCell().getCellStyle().getDataFormatString()) && c.getCell().getCellStyle().getDataFormatString().contains(dataFormatTypeFlag)) {
+                dataFormatType = c.getCell().getCellStyle().getDataFormatString().replace(dataFormatTypeFlag, "").replace("\"", "");
+            }
+            if (StrUtil.isNotEmpty(dataFormatType) && c.getCell().getCellType().equals(CellType.NUMERIC)) {
+                String dataFormat0 = "#0";
+                String dataFormat1 = "#0.0";
+                String dataFormat2 = "#0.00";
+                String dataFormat3 = "#0.000";
+                String dataFormat = null;
+                if (dataFormatType.contains("water_flow")) {
+                    double cellValue = Math.abs(c.getCell().getNumericCellValue());
+                    if (cellValue < 1) {
+                        dataFormat = dataFormat3;
+                    } else if (cellValue < 10) {
+                        dataFormat = dataFormat2;
+                    } else if (cellValue < 100) {
+                        dataFormat = dataFormat1;
+                    } else {
+                        dataFormat = dataFormat0;
+                    }
+                }
+                if (StrUtil.isNotEmpty(dataFormat)) {
+                    fillCellStyle(c, dataFormat, cellStyleMap);
+                    c.getCell().getCellStyle().setDataFormat(c.getWorkbook().createDataFormat().getFormat(dataFormat));
                 }
             }
         });
+    }
+
+    public static void fillCellStyle(PoiUtils.CellContext c, String key, Map<String, CellStyle> cellStyleMap) {
+        CellStyle newCellStyle;
+        if (cellStyleMap.containsKey(key)) {
+            newCellStyle = cellStyleMap.get(key);
+        } else {
+            // 创建新的单元格样式，不然对当前单元格样式的修改会作用到其他的单元格
+            CellStyle oldCellStyle = c.getCell().getCellStyle();
+            newCellStyle = c.getWorkbook().createCellStyle();
+            newCellStyle.cloneStyleFrom(oldCellStyle);
+            cellStyleMap.put(key, newCellStyle);
+        }
+        c.getCell().setCellStyle(newCellStyle);
     }
 
 }
