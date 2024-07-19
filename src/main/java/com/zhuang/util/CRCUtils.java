5f96d7bf1ea1679eee8e70a78025c5d07e405d23
庄伟斌 <zhuangweibin@qq.com>
2024-07-19 15:33:30 +0800

    aa

--- src/main/java/com/zhuang/util/CRCUtils.java
+++ src/main/java/com/zhuang/util/CRCUtils.java
@@ -6,12 +6,19 @@ package com.zhuang.util;
 public class CRCUtils {
 
     private static final int POLYNOMIAL_16 = 0xA001;
+    private static final short POLYNOMIAL_8 = 0x07; // CRC8多项式，也可以使用其他的多项式
+    private static final short POLYNOMIAL_7 = 0xe5; // CRC7多项式，也可以使用其他的多项式
 
     public static int getCRC16(byte[] bytes) {
+        return getCRC16(bytes, 0, bytes.length);
+    }
+
+    public static int getCRC16(byte[] bytes, int pos, int length) {
         int crc = 0xFFFF;
-        for (byte b : bytes) {
+        for (int i = pos; i < length; i++) {
+            byte b = bytes[i];
             crc ^= (int) b & 0xFF;
-            for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 if ((crc & 1) == 1) {
                     crc = (crc >>> 1) ^ POLYNOMIAL_16;
                 } else {
@@ -22,18 +29,20 @@ public class CRCUtils {
         return crc;
     }
 
-    private static final short POLYNOMIAL_8 = 0x07; // CRC8多项式，也可以使用其他的多项式
-    private static final short POLYNOMIAL_7 = 0xe5; // CRC7多项式，也可以使用其他的多项式
-
     public static byte getCRC8(byte[] bytes) {
         return getCRC8(bytes, POLYNOMIAL_8);
     }
 
     public static byte getCRC8(byte[] bytes, short polynomial) {
+        return getCRC8(bytes, 0, bytes.length, polynomial);
+    }
+
+    public static byte getCRC8(byte[] bytes, int pos, int length, short polynomial) {
         short crc = 0x00; // CRC8校验值初始值
-        for (byte b : bytes) {
+        for (int i = pos; i < length; i++) {
+            byte b = bytes[i];
             crc ^= b; // 异或
-            for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 if ((crc & 0x80) != 0) {
                     crc = (short) ((crc << 1) ^ polynomial); // 左移并异或
                 } else {
