8346bc6b9466fae69b5964eddfdf2905263ad0c3
庄伟斌 <zhuangweibin@qq.com>
2024-09-02 14:51:54 +0800

    aa

--- src/main/java/com/zhuang/util/spring/RedisUtils.java
+++ src/main/java/com/zhuang/util/spring/RedisUtils.java
@@ -1,6 +1,9 @@
 package com.zhuang.util.spring;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.zhuang.util.jackson.JacksonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +12,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -34,22 +34,25 @@ public class RedisUtils {
         _this.stringRedisTemplate.convertAndSend(channel, message);
     }
 
-    public static void set(String key, String value) {
-        _this.stringRedisTemplate.opsForValue().set(key, value);
+    public static void set(String key, Object value) {
+        _this.stringRedisTemplate.opsForValue().set(key, objectToString(value));
     }
 
-    public static void set(String key, String value, int timeoutSeconds) {
+    public static void set(String key, Object value, int timeoutSeconds) {
         set(key, value, timeoutSeconds, TimeUnit.SECONDS);
     }
 
-    public static void set(String key, String value, long timeout, TimeUnit timeUnit) {
-        _this.stringRedisTemplate.opsForValue().set(key, value, timeout, timeUnit);
+    public static void set(String key, Object value, long timeout, TimeUnit timeUnit) {
+        _this.stringRedisTemplate.opsForValue().set(key, objectToString(value), timeout, timeUnit);
     }
 
     public static String get(String key) {
         return _this.stringRedisTemplate.opsForValue().get(key);
     }
 
+    public static <T> T get(String key, Class<T> clazz) {
+        return stringToObject(get(key), clazz);
+    }
 
     public static void delete(String key) {
         _this.stringRedisTemplate.delete(key);
@@ -80,22 +83,22 @@ public class RedisUtils {
         return _this.stringRedisTemplate.getConnectionFactory().getConnection().getNativeConnection();
     }
 
-    public static List<String> getByPattern(String pattern) {
+    public static List<String> getListByPattern(String pattern) {
         Set<String> keys = _this.stringRedisTemplate.keys(pattern);
         return keys.stream().map(RedisUtils::get).collect(Collectors.toList());
     }
 
-    public static <T> List<T> getByPattern(String pattern, Class<T> clazz) {
-        List<String> stringList = getByPattern(pattern);
-        return stringList.stream().filter(c -> StrUtil.isNotEmpty(c)).map(c -> {
+    public static <T> List<T> getListByPattern(String pattern, Class<T> clazz) {
+        List<String> stringList = getListByPattern(pattern);
+        return stringList.stream().filter(StrUtil::isNotEmpty).map(c -> {
             T t = null;
             try {
                 t = JacksonUtils.toBean(c, clazz);
             } catch (Exception ex) {
-                log.error("RedisUtils.getByPattern error!", ex);
+                log.error("RedisUtils.getListByPattern error!", ex);
             }
             return t;
-        }).filter(c -> c != null).collect(Collectors.toList());
+        }).filter(Objects::nonNull).collect(Collectors.toList());
     }
 
 
@@ -107,8 +110,12 @@ public class RedisUtils {
         return opsForHash().get(key, hashKey);
     }
 
-    public static void putForHash(String key, String hashKey, String value) {
-        opsForHash().put(key, hashKey, value);
+    public static <T> T getForHash(String key, String hashKey, Class<T> clazz) {
+        return stringToObject(getForHash(key, hashKey), clazz);
+    }
+
+    public static void putForHash(String key, String hashKey, Object value) {
+        opsForHash().put(key, hashKey, objectToString(value));
     }
 
     public static void putAllForHash(String key, Map<String, String> map) {
@@ -129,6 +136,24 @@ public class RedisUtils {
 
     public static void expireForHash(String key, long timeout, TimeUnit unit) {
         opsForHash().getOperations().expire(key, timeout, unit);
+    }
+
+    private static String objectToString(Object value) {
+        String strValue;
+        if (value == null) return null;
+        if (ClassUtil.isPrimitiveWrapper(value.getClass())) {
+            strValue = value.toString();
+        } else if (value instanceof Date) {
+            strValue = DateUtil.formatDateTime((Date) value);
+        } else {
+            strValue = JacksonUtils.toJsonStr(value);
+        }
+        return strValue;
+    }
+
+    private static <T> T stringToObject(String s, Class<T> clazz) {
+        if (StrUtil.isEmpty(s)) return null;
+        return JacksonUtils.toBean(s, clazz);
     }
 
 }
