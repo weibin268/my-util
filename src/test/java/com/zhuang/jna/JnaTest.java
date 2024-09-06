91afd9e2da37cce0fb63b815e1d9172674e3ee04
庄伟斌 <zhuangweibin@qq.com>
2024-09-06 17:49:32 +0800

    aa

--- src/test/java/com/zhuang/jna/JnaTest.java
+++ src/test/java/com/zhuang/jna/JnaTest.java
@@ -2,7 +2,13 @@ package com.zhuang.jna;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.ptr.PointerByReference;
+import com.zhuang.util.ByteUtils;
 import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class JnaTest {
     @Test
@@ -16,4 +22,23 @@ public class JnaTest {
     public interface Kernel32 extends Library {
         boolean DeleteFileA(String fileName);
     }
+
+    @Test
+    public void AdcpToolForWin() {
+        System.setProperty("jna.library.path", "D:\\zhuang\\doc\\ADCP\\C++\\Debug");
+        AdcpToolForWin kernel32 = (AdcpToolForWin) Native.loadLibrary("AdcpToolForWin", AdcpToolForWin.class);
+        PointerByReference b = kernel32.AdcpToolRequest("AutoRating", " {\n" +
+                "            velocity:[], \n" +
+                "            velocityOnline:[], \n" +
+                "            waterOnline:[], \n" +
+                "            model:1\n" +
+                "        }");
+        System.out.println(b.getPointer().getString(0, "GBK"));
+    }
+
+    public interface AdcpToolForWin extends Library {
+        PointerByReference AdcpToolRequest(String method, String jsonArgs);
+    }
 }
+
+
