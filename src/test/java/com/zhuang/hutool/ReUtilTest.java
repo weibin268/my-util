2c7c918d16a3ce3e9aaca3b19e2f95e909363afa
zwb <binweizhuang@hotmail.com>
2024-11-27 21:52:26 +0800

    aa

--- src/test/java/com/zhuang/hutool/ReUtilTest.java
+++ src/test/java/com/zhuang/hutool/ReUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ReUtilTest {
 
     @Test
-    public void get(){
-        System.out.println(ReUtil.get("(7E|68)+[0-9A-F]+", "20241127154101:已下发指令到设备[4452812005]，平台发送报文HEX内容：680768304452812005E2CF16", 0));
+    public void getGroup0(){
+        System.out.println(ReUtil.getGroup0("(7E|68)+[0-9A-F]+", "20241127154101:已下发指令到设备[4452812005]，平台发送报文HEX内容：680768304452812005E2CF16"));
     }
 }
