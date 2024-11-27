2fc83dd2f4e2151e59fd6c2271bffeefe7d88403
zwb <binweizhuang@hotmail.com>
2024-11-27 22:03:05 +0800

    aa

--- src/test/java/com/zhuang/hutool/ReUtilTest.java
+++ src/test/java/com/zhuang/hutool/ReUtilTest.java
@@ -6,7 +6,14 @@ import org.junit.Test;
 public class ReUtilTest {
 
     @Test
-    public void getGroup0(){
+    public void getGroup0() {
+        // 提取7E或68开头的16进制字符串
         System.out.println(ReUtil.getGroup0("(7E|68)+[0-9A-F]+", "20241127154101:已下发指令到设备[4452812005]，平台发送报文HEX内容：680768304452812005E2CF16"));
+    }
+
+    @Test
+    public void delAll() {
+        // 删除中括号及里面的内容
+        System.out.println(ReUtil.delAll("\\[(.*?)\\]", "aa[4452812005]bb[1234567890]cc"));
     }
 }
