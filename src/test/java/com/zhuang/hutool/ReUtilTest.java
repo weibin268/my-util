7cfd6e0fde8290b3a8e880190b95cebda2bee449
zwb <binweizhuang@hotmail.com>
2024-11-27 22:24:05 +0800

    aa

--- src/test/java/com/zhuang/hutool/ReUtilTest.java
+++ src/test/java/com/zhuang/hutool/ReUtilTest.java
@@ -13,6 +13,7 @@ public class ReUtilTest {
         String tpl1 = "{}.{}：{}";
         String tpl2 = "原：{}\n新：{}";
         System.out.println(StrUtil.repeat('-', length));
+
         String title = "提取7E或68开头的16进制字符串";
         String pattern = "(7E|68)+[0-9A-F]+";
         String content = "20241127154101:已下发指令到设备[4452812005]，平台发送报文HEX内容：680768304452812005E2CF16";
@@ -23,7 +24,17 @@ public class ReUtilTest {
 
     @Test
     public void delAll() {
-        // 删除中括号及里面的内容
-        System.out.println(ReUtil.delAll("\\[(.*?)\\]", "aa[4452812005]bb[1234567890]cc"));
+        int index = 0;
+        int length = 100;
+        String tpl1 = "{}.{}：{}";
+        String tpl2 = "原：{}\n新：{}";
+        System.out.println(StrUtil.repeat('-', length));
+
+        String title = "删除中括号及里面的内容";
+        String pattern = "\\[(.*?)\\]";
+        String content = "aa[4452812005]bb[1234567890]cc";
+        System.out.println(StrUtil.format(tpl1, (++index), title, pattern));
+        System.out.println(StrUtil.format(tpl2, content, ReUtil.delAll(pattern, content)));
+        System.out.println(StrUtil.repeat('-', length));
     }
 }
